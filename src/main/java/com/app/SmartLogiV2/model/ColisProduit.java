736fc0d464ac9fd3c9a0c84f9bdc30424cb0880c
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-10-27 15:04:17 +0100

    feat: create ColisProduit that represent many to many relationship between produit and colis,with composite key

---
+++ src/main/java/com/app/SmartLogiV2/model/ColisProduit.java
@@ -0,0 +1,73 @@
+package com.app.SmartLogiV2.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class ColisProduit {
+    @EmbeddedId
+    ColisProduitKey id;
+
+    @ManyToOne
+    @MapsId("colisId")
+    @JoinColumn(name = "colis_id")
+    private Colis colis;
+
+    @ManyToOne
+    @MapsId("produitId")
+    @JoinColumn(name = "produit_id")
+    private Produit produit;
+
+    private int Quantite;
+    private float prix;
+    private LocalDate dateAjout;
+
+    public ColisProduitKey getId() {
+        return id;
+    }
+
+    public void setId(ColisProduitKey id) {
+        this.id = id;
+    }
+
+    public Colis getColis() {
+        return colis;
+    }
+
+    public void setColis(Colis colis) {
+        this.colis = colis;
+    }
+
+    public Produit getProduit() {
+        return produit;
+    }
+
+    public void setProduit(Produit produit) {
+        this.produit = produit;
+    }
+
+    public int getQuantite() {
+        return Quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        Quantite = quantite;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public LocalDate getDateAjout() {
+        return dateAjout;
+    }
+
+    public void setDateAjout(LocalDate dateAjout) {
+        this.dateAjout = dateAjout;
+    }
+}
--- src/main/java/com/app/SmartLogiV2/model/Produit.java
+++ src/main/java/com/app/SmartLogiV2/model/Produit.java
@@ -12,4 +12,47 @@ public class Produit {
     private String nom;
     private String categorie;
     private Float poids;
+
+    @ManyToMany
+    @JoinTable(name = "colis_produit")
+    private Colis colis;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Float prix) {
+        this.prix = prix;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public Float getPoids() {
+        return poids;
+    }
+
+    public void setPoids(Float poids) {
+        this.poids = poids;
+    }
 }
