6d0f4ea15f9f2496a50385fb4ca08bb1c9d69997
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-10-27 11:33:59 +0100

    update project

---
+++ src/main/java/com/app/SmartLogiV2/SmartLogiV2Application.java
@@ -0,0 +1,13 @@
+package com.app.SmartLogiV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLogiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/SmartLogiV2/SmartLogiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SmartLogiV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLogiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
