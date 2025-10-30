8f029d3f0264083601b8d88b8ff0022f5013cb6e
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-10-30 11:02:34 +0100

    feat : create a LivreurNotFoundException exception class to handle Livreur not found Exception

---
+++ src/main/java/com/app/SmartLogiV2/exception/LivreurNotFoundExpception.java
@@ -0,0 +1,7 @@
+package com.app.SmartLogiV2.exception;
+
+public class LivreurNotFoundExpception extends RuntimeException {
+    public LivreurNotFoundExpception(String message) {
+        super(message);
+    }
+}
