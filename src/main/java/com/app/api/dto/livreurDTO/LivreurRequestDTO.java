9458360926f6534adf4b49ed6ec30f612d56f9bc
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-10-30 16:20:25 +0100

    feat : add validation to livreur request dto

--- src/main/java/com/app/api/dto/livreurDTO/LivreurRequestDTO.java
+++ src/main/java/com/app/api/dto/livreurDTO/LivreurRequestDTO.java
@@ -1,11 +1,17 @@
 package com.app.api.dto.livreurDTO;
 
-public class LivreurRequestDTO {
+import jakarta.validation.constraints.*;
 
+public class LivreurRequestDTO {
+    @NotBlank(message = "entrer un nom")
     private String nom;
+    @NotBlank(message = "enter un prenom")
     private String prenom;
+    @Email(message = "entrer un email valide")
     private String email;
+    @Pattern(regexp = "/\\+0[0-9]{9}",message = "telephone invalid (format : 0 + 9 nombre apré , ex : 0678954321")
     private String telephone;
+    @NotBlank(message = "entre une vehicule")
     private String vehicule;
     private Long zoneId;
 
