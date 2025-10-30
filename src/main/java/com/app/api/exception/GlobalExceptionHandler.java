d154cf3ddda8542763071cb2762403dda4ffc102
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-10-30 15:10:59 +0100

    feat : intercept InvalidDataException in global exceptions handler

--- src/main/java/com/app/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/api/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.app.api.exception;
 
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +13,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex){
         return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidDataException.class)
+    public ResponseEntity<String> handleInvalidData(InvalidDataException ex){
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_ACCEPTABLE);
     }
 }
