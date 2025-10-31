1f3ab0562d8708c75791a115c22136aa751abdb5
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-10-31 10:47:25 +0100

    feat : implement colis request dto

--- src/main/java/com/app/api/dto/colisDTO/ColisRequestDTO.java
+++ src/main/java/com/app/api/dto/colisDTO/ColisRequestDTO.java
@@ -1,4 +1,129 @@
 package com.app.api.dto.colisDTO;
 
+import com.app.api.entity.*;
+import com.app.api.enums.ColisPriority;
+import com.app.api.enums.ColisStatus;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
 public class ColisRequestDTO {
+
+    private String id;
+    private Float poids;
+    private String description;
+    private String adresse;
+    private String villeDestination;
+    private ColisPriority priority;
+    private ColisStatus status;
+    private String livreurId;
+    private Set<ColisProduit> colisProduits;
+    private Set<HistoriqueLivraison> historiqueLivraison;
+    private Expediteur expediteurId;
+    private Destinataire destinataireId;
+    private Zone zoneId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Float getPoids() {
+        return poids;
+    }
+
+    public void setPoids(Float poids) {
+        this.poids = poids;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getVilleDestination() {
+        return villeDestination;
+    }
+
+    public void setVilleDestination(String villeDestination) {
+        this.villeDestination = villeDestination;
+    }
+
+    public ColisPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(ColisPriority priority) {
+        this.priority = priority;
+    }
+
+    public ColisStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ColisStatus status) {
+        this.status = status;
+    }
+
+    public String getLivreurId() {
+        return livreurId;
+    }
+
+    public void setLivreurId(String livreurId) {
+        this.livreurId = livreurId;
+    }
+
+    public Set<ColisProduit> getColisProduits() {
+        return colisProduits;
+    }
+
+    public void setColisProduits(Set<ColisProduit> colisProduits) {
+        this.colisProduits = colisProduits;
+    }
+
+    public Set<HistoriqueLivraison> getHistoriqueLivraison() {
+        return historiqueLivraison;
+    }
+
+    public void setHistoriqueLivraison(Set<HistoriqueLivraison> historiqueLivraison) {
+        this.historiqueLivraison = historiqueLivraison;
+    }
+
+    public Expediteur getExpediteurId() {
+        return expediteurId;
+    }
+
+    public void setExpediteurId(Expediteur expediteurId) {
+        this.expediteurId = expediteurId;
+    }
+
+    public Destinataire getDestinataireId() {
+        return destinataireId;
+    }
+
+    public void setDestinataireId(Destinataire destinataireId) {
+        this.destinataireId = destinataireId;
+    }
+
+    public Zone getZoneId() {
+        return zoneId;
+    }
+
+    public void setZoneId(Zone zoneId) {
+        this.zoneId = zoneId;
+    }
 }
