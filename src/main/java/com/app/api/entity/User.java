c04fb8d49b218e681634ac628dfe3254840ca887
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-11-19 12:25:35 +0100

    feat: create User entity to represent authenticated user entity

---
+++ src/main/java/com/app/api/entity/User.java
@@ -0,0 +1,30 @@
+package com.app.api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Table(name = "users")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private String userId;
+    @Column(unique = true,length = 100,nullable = false)
+    private String email;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Date createdAt;
+    @UpdateTimestamp
+    private Date updatedAt;
+}
