ee3bbc0acbeaecea8158f9957d5bbd79bef4fd1f
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-17 12:28:21 +0100

    feat: implement user service ,load user by username(email) method , add new user method

---
+++ src/main/java/com/app/api/security/service/UserService.java
@@ -0,0 +1,32 @@
+package com.app.api.security.service;
+
+import com.app.api.entity.UserInfo;
+import com.app.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserInfo user = userRepository.findByEmail(email).orElseThrow(
+                ()-> new UsernameNotFoundException("user not found with email : "+email)
+        );
+
+        return new UserInfoDetails(user);
+    }
+
+    public String addUser(UserInfo userInfo) {
+        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+        userRepository.save(userInfo);
+        return "User added successfully!";
+    }
+}
