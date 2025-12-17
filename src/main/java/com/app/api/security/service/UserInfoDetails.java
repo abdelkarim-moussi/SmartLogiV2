3408d2913ea080c45dac8ddfab86d5003ca43e3d
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-17 12:29:12 +0100

    feat: implement user detail for carying user details isolated form the entity

---
+++ src/main/java/com/app/api/security/service/UserInfoDetails.java
@@ -0,0 +1,42 @@
+package com.app.api.security.service;
+
+import com.app.api.entity.UserInfo;
+import com.app.api.enums.UserRole;
+import lombok.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserInfoDetails implements UserDetails {
+    private String userEmail;
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public UserInfoDetails(UserInfo user) {
+        this.userEmail = user.getEmail(); // Use email as username
+        this.password = user.getPassword();
+        this.authorities = Arrays.stream(user.getRole().toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userEmail;
+    }
+}
