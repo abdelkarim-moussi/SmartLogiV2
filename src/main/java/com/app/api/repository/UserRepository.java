37bf26df16a206bc46f58bb04906f63b0dd6161e
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-17 12:27:06 +0100

    refactor: fix Entity error

--- src/main/java/com/app/api/repository/UserRepository.java
+++ src/main/java/com/app/api/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.app.api.repository;
 
-import com.app.api.entity.User;
+import com.app.api.entity.UserInfo;
+import com.app.api.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,String> {
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<UserInfo,String> {
+    Optional<UserInfo> findByEmail(String email);
 }
