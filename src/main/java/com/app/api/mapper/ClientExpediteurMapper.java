70e56c4791171c809fb8d309d650c3d7f95d81ef
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-19 20:09:26 +0100

    refactor: update mappers for client and livreur

--- src/main/java/com/app/api/mapper/ClientExpediteurMapper.java
+++ src/main/java/com/app/api/mapper/ClientExpediteurMapper.java
@@ -4,10 +4,12 @@ import com.app.api.dto.clientExpediteurDTO.ClientExpediteurRequestDTO;
 import com.app.api.dto.clientExpediteurDTO.ClientExpediteurResponseDTO;
 import com.app.api.entity.ClientExpediteur;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ClientExpediteurMapper {
-
+    @Mapping(target = "colisEnvoyer", ignore = true)
+    @Mapping(target = "user", ignore = true)
     ClientExpediteur toEntity(ClientExpediteurRequestDTO clientExpediteurRequestDTO);
     ClientExpediteurResponseDTO toDTO(ClientExpediteur client);
 }
