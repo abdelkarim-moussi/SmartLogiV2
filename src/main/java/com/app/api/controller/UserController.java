c7284dee2ac2097494c9c26b17e8b8df4de61e80
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2025-12-19 11:33:27 +0100

    refactor: remove authentication logic from controller to the Auth Service

--- src/main/java/com/app/api/controller/UserController.java
+++ src/main/java/com/app/api/controller/UserController.java
@@ -2,15 +2,10 @@ package com.app.api.controller;
 
 import com.app.api.dto.user.AuthRequest;
 import com.app.api.dto.user.CreateUserRequest;
-import com.app.api.security.service.JwtService;
-import com.app.api.security.service.UserDetailsImpl;
+import com.app.api.security.service.AuthService;
 import com.app.api.security.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,22 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-    private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
+    private final AuthService authService;
 
     @PostMapping("/login")
     public String authenticateAndGetToken(@RequestBody @Valid AuthRequest authRequest){
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        authRequest.getUserEmail(),authRequest.getPassword())
-        );
-
-        if(authentication.isAuthenticated()){
-            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-            return jwtService.generateToken(userDetails);
-        }else {
-            throw new UsernameNotFoundException("Invalid user request");
-        }
+        return authService.authenticate(authRequest);
     }
 
     @PostMapping("/addNewUser")
