afa17899cdd54de9d68bac2b28ea3f51e22c385e
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-05 23:20:59 +0000

    exclode OAuth2Client from auto config

--- src/test/java/com/app/api/SmartLogiV2ApplicationTests.java
+++ src/test/java/com/app/api/SmartLogiV2ApplicationTests.java
@@ -1,11 +1,15 @@
 package com.app.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration.class
+})
 class SmartLogiV2ApplicationTests {
 
 	@Test
