3c9e56aa0155830838489dbe5d38e83bf9fa985c
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2026-01-06 11:22:35 +0000

    update Jenkins file

--- src/main/java/com/app/api/security/config/SecurityConfig.java
+++ src/main/java/com/app/api/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.app.api.security.service.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
--- src/test/java/com/app/api/SmartLogiV2ApplicationTests.java
+++ src/test/java/com/app/api/SmartLogiV2ApplicationTests.java
@@ -7,9 +7,6 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration.class
-})
 class SmartLogiV2ApplicationTests {
 
 	@Test
